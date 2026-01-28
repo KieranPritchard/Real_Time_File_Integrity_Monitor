14d6575b940cc27623a82f2e081a6dae19041c8b
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-28 19:48:16 +0000

    created structs

--- real_time_file_integrity_monitor.go
+++ real_time_file_integrity_monitor.go
@@ -11,3 +11,15 @@ import (
 	"time"
 	"github.com/fsnotify/fsnotify"
 )
+
+// Structure to store file state
+type FileState struct {
+	// Stores the files hash
+	Hash string
+}
+
+// Stores the job information
+type Job struct {
+	Path string // Stores the file path
+	Op   fsnotify.Op // Stores the operation of the fsnotify
+}
