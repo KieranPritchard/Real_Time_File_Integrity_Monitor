d6fde894ef5f8a1b5407752b3d83b3616b2348b7
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-28 20:05:55 +0000

    added hashfile function

--- real_time_file_integrity_monitor.go
+++ real_time_file_integrity_monitor.go
@@ -23,3 +23,26 @@ type Job struct {
 	Path string // Stores the file path
 	Op   fsnotify.Op // Stores the operation of the fsnotify
 }
+
+// Function to hash files
+func hashFile(path string) (string, error) {
+	// Opens the file from the path and gets the error
+	file, err := os.Open(path)
+	// Checks if the error is not nil
+	if err != nil {
+		// returns a empty strng and the error
+		return "", err
+	}
+	// Closes the file when done
+	defer file.Close()
+
+	// Creates new hash
+	hash := sha256.New()
+	// Checks if the io copy is hash and a file and checks if error isnnt ill
+	if _, err := io.Copy(hash, file); err != nil {
+		// returns a empty strng and the error
+		return "", err
+	}
+	// Returns the hex value of the file hash
+	return hex.EncodeToString(hash.Sum(nil)), nil
+}
