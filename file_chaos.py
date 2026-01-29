b1241e79fd49c518c9329100f4000a20491d5a4e
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-29 20:55:17 +0000

    added main function

--- file_chaos.py
+++ file_chaos.py
@@ -31,3 +31,77 @@ def list_files():
     files = [os.path.join(TARGET_DIR, f) for f in os.listdir(TARGET_DIR) if os.path.isfile(os.path.join(TARGET_DIR, f))]
     # Returns the files
     return files
+
+# Function to create file
+def create_file():
+    # Creates the random files path
+    path = os.path.join(TARGET_DIR, random_filename())
+    # Opens the new file at the path
+    with open(path, "w") as f:
+        # Writes content from the random content function to the file
+        f.write(random_content())
+    # Logs a file has been created
+    print(f"[CREATE] {path}")
+
+# Function to modify file
+def modify_file(path):
+    # Opens the file path in the parameter
+    with open(path, "a") as f:
+        # Writes adds extra content to files
+        f.write("\n" + random_content())
+    # Outputs the path that has been modified
+    print(f"[MODIFY] {path}")
+
+# Function to overright file
+def overwrite_file(path):
+    # Opens the new file at the path
+    with open(path, "w") as f:
+        # Writes content from the random content function to the file
+        f.write(random_content())
+    # Outputs the path that has been modified
+    print(f"[OVERWRITE] {path}")
+
+# Function to delete file
+def delete_file(path):
+    # Removes the file at the path
+    os.remove(path)
+    # Outputs the delete file function
+    print(f"[DELETE] {path}")
+
+# Main function
+def main():
+    # Runs while true
+    while True:
+        # Lists the files in the directory
+        files = list_files()
+        # Chooses a random action
+        action = random.choice(["create", "modify", "overwrite", "delete"])
+
+        # Checks if the action is create and the length of files is less than the max amount
+        if action == "create" and len(files) < MAX_FILES:
+            # Creates a random file
+            create_file()
+
+        # Checks if the action is in modify and overright and if there are files
+        elif action in ("modify", "overwrite") and files:
+            # Chooses a random file
+            path = random.choice(files)
+            # Checsk if the action is modify
+            if action == "modify":
+                # Calls the modify file function with the path
+                modify_file(path)
+            else:
+                # Calls the overwrite file path
+                overwrite_file(path)
+
+        # Checks for the delete action and if there are files
+        elif action == "delete" and files:
+            # Deletes a random file
+            delete_file(random.choice(files))
+        # Sleeps for a random amount of time
+        time.sleep(random.uniform(SLEEP_MIN, SLEEP_MAX))
+
+# Starts the program
+if __name__ == "__main__":
+    print("🔥 File chaos generator started")
+    main()
